36ba437680786cb8916030a87a7706b89fd12236
Adler Eduardo <adlereduardo@icloud.com>
2024-11-20 02:49:36 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { MissaoComponent } from './missao/missao.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AboutUsComponent, CarouselComponent, ContactFormComponent, FirstPageComponent, MissaoComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
